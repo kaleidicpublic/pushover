c3e82701d88a6ffa4c06ef5e9700cae4790721ac
Laeeth Isharc <laeeth@laeeth.com>
2017-04-21 09:15:52 +0100

    docs

--- source/pushover.d
+++ source/pushover.d
@@ -1,3 +1,46 @@
+/**
+    Implemented in the D Programming Language 2016 by Laeeth Isharc and Kaleidic Associates Advisory UK Limited
+    Boost Licensed
+    Use at your own risk - this is not tested at all.
+
+    API for pushover notification API
+    https://pushover.net/
+*/
+/**
+    Example:
+    ---
+        import kaleidic.api.pushover;
+        import std.datetime:Clock;
+
+        enum applicationKey = "set me".ApplicationToken;
+        enum targetUserKey = "set me".UserKey;
+        enum groupKey = "set me".GroupKey;
+        enum targetUserMemo ="memo field here";
+        
+        auto api=PushoverAPI(applicationToken);
+        writefln("validate target user: %s",api.validate(targetUserKey));
+        writeln("result of adding target user to group:",
+            api.addUserToGroup(
+                        targetUserKey,
+                        groupKey,
+                        null.DeviceName,
+                        targetUserMemo)
+        );
+        PushoverMessage message;
+
+        message=message.setMessage("as the CNBC anchor said, is buying GS here like D&G on sale?")
+            .setTitle("Kaleidic Market Alert - GS")
+            .setUrl("kaleidic.io")
+            .setUrlTitle("GS chart")
+            .setPriority(PushoverMessagePriority.high)
+            .setTimeStamp(Clock.currTime());
+        writefln("%s",message);
+        auto ret=api.sendMessage(message,targetUserKey);
+        writefln("message status: %s",ret["status"]);
+        writefln("message request: %s",ret["request"]);
+    ---
+*/
+
 module kaleidic.api.pushover;
 import std.stdio;
 import std.json;
@@ -11,16 +54,8 @@ import std.format:format;
 import std.variant:Algebraic;
 import std.typecons:Nullable;
 import std.datetime:SysTime,DateTime;
-/**
-    Implemented in the D Programming Language 2016 by Laeeth Isharc and Kaleidic Associates Advisory UK Limited
-    Boost Licensed
-    Use at your own risk - this is not tested at all.
 
-    API for pushover notification API
-    https://pushover.net/
-*/
-debug=1;
-
+///
 static this()
 {
     PushoverMessageSounds=[ "pushover",
@@ -48,6 +83,7 @@ static this()
 
 }
 
+///
 string joinUrl(string url, string endpoint)
 {
     enforce(url.length>0, "broken url");
@@ -56,34 +92,42 @@ string joinUrl(string url, string endpoint)
     return url~"/"~endpoint;
 }
 
+///
 struct ApplicationKey
 {
     string key;
     alias key this;
 }
+
+///
 struct UserKey
 {
     string key;
     alias key this;
 }
 
+///
 struct GroupKey
 {
     string key;
     alias key this;
 }
 
+///
 struct APIToken
 {
     string token;
     alias token this;
 }
 
+///
 struct DeviceName
 {
     string name;
     alias name this;
 }
+
+///
 struct PushoverAPI
 {
     string endpoint = "https://api.pushover.net/1/";
@@ -101,6 +145,7 @@ struct PushoverAPI
     }
 }
 
+///
 enum PushoverMessagePriority
 {
     lowest=-2,
@@ -110,8 +155,10 @@ enum PushoverMessagePriority
     emergency=2, 
 }
 
+///
 string[] PushoverMessageSounds;
 
+///
 struct PushoverMessage
 {
     string messageText=null;
@@ -129,65 +176,77 @@ struct PushoverMessage
     }
 }
 
+///
 auto ref setMessage(ref PushoverMessage message, string messageText)
 {
     message.messageText=messageText;
     return message;
 }
 
+///
 auto ref setDevice(ref PushoverMessage message, DeviceName device)
 {
     message.device=device.name;
     return message;
 }
 
+///
 auto ref setTitle(ref PushoverMessage message, string title)
 {
     message.title=title;
     return message;
 }
 
+///
 auto ref setUrl(ref PushoverMessage message, string url)
 {
     message.url=url;
     return message;
 }
 
+///
 auto ref setUrlTitle(ref PushoverMessage message, string urlTitle)
 {
     message.urlTitle=urlTitle;
     return message;
 }
+
+///
 auto ref setPriority(ref PushoverMessage message, PushoverMessagePriority priority)
 {
     message.priority=priority;
     return message;
 }
 
+///
 auto ref setPriority(ref PushoverMessage message, int priority)
 {
     message.priority=priority.to!PushoverMessagePriority;
     return message;
 }
 
+///
 auto ref setTimeStamp(ref PushoverMessage message, DateTime timeStamp)
 {
     message.timeStamp=cast(SysTime) timeStamp;
     return message;
 }
 
+///
 auto ref setTimeStamp(ref PushoverMessage message, SysTime timeStamp)
 {
     message.timeStamp=timeStamp;
     return message;
 }
 
+///
 auto ref setSound(ref PushoverMessage message, string sound)
 {
     message.sound=sound;
     return message;
 }
 
+///
 auto sendMessage(PushoverAPI api, PushoverMessage message, UserKey user=null.UserKey)
 {
     JSONValue params = ["message": message.messageText];
@@ -217,12 +276,14 @@ auto sendMessage(PushoverAPI api, PushoverMessage message, UserKey user=null.Use
     return api.request("messages.json", HTTP.Method.post,params);
 }
 
+///
 auto listGroupMembers(PushoverAPI api, GroupKey groupKey)
 {
     return api.request("groups/"~groupKey~".json",HTTP.Method.get);
 }
 
 
+///
 auto addUserToGroup(PushoverAPI api, UserKey userKey, GroupKey groupKey, DeviceName device=null.DeviceName, string memo=null)
 {
     import std.uri:encodeComponent;
@@ -235,6 +296,7 @@ auto addUserToGroup(PushoverAPI api, UserKey userKey, GroupKey groupKey, DeviceN
     return api.request("groups/"~groupKey.key.encodeComponent~"/add_user.json",HTTP.Method.post,params);
 }
 
+///
 auto removeUserFromGroup(PushoverAPI api, UserKey userKey, GroupKey groupKey)
 {
     import std.uri:encodeComponent;
@@ -243,6 +305,7 @@ auto removeUserFromGroup(PushoverAPI api, UserKey userKey, GroupKey groupKey)
     return api.request("groups/"~groupKey.key.encodeComponent~"/delete_user.json",HTTP.Method.post,params);
 }
 
+///
 auto disableUser(PushoverAPI api, UserKey userKey, GroupKey groupKey)
 {
     import std.uri:encodeComponent;
@@ -251,6 +314,7 @@ auto disableUser(PushoverAPI api, UserKey userKey, GroupKey groupKey)
     return api.request("groups/"~groupKey.key.encodeComponent~"/disable_user.json",HTTP.Method.post,params);
 }
 
+///
 auto enableUser(PushoverAPI api, UserKey userKey, GroupKey groupKey)
 {
     import std.uri:encodeComponent;
@@ -258,6 +322,7 @@ auto enableUser(PushoverAPI api, UserKey userKey, GroupKey groupKey)
     return api.request("groups/"~groupKey.key.encodeComponent~"/enable_user.json",HTTP.Method.post,params);
 }
 
+///
 auto renameGroup(PushoverAPI api, string oldName, string newName)
 {
     import std.uri:encodeComponent;
@@ -265,11 +330,14 @@ auto renameGroup(PushoverAPI api, string oldName, string newName)
     params["name"]=newName;
     return api.request("groups/"~oldName.encodeComponent~"/rename.json",HTTP.Method.post,params);
 }
+
+///
 auto listSounds(PushoverAPI api)
 {
     return api.request("sounds.json",HTTP.Method.get);
 }
 
+///
 auto validate(PushoverAPI api, UserKey user, DeviceName device=null.DeviceName)
 {
     JSONValue params;
@@ -279,18 +347,21 @@ auto validate(PushoverAPI api, UserKey user, DeviceName device=null.DeviceName)
     return api.request("users/validate.json",HTTP.Method.post,params);
 }
 
+///
 auto checkReceipt(PushoverAPI api, string receipt)
 {
     import std.uri:encodeComponent;
     return api.request("receipts/"~receipt.encodeComponent~".json");
 }
 
+///
 auto cancelEmergencyDelivery(PushoverAPI api, string receipt)
 {
     import std.uri:encodeComponent;
     return api.request("receipts/"~receipt.encodeComponent~"/cancel.json");
 }
 
+///
 auto assignLicense(PushoverAPI api, string email=null, string os=null)
 {
     JSONValue params;
@@ -301,6 +372,7 @@ auto assignLicense(PushoverAPI api, string email=null, string os=null)
     return api.request("licenses/assign.json");
 }
 
+///
 string stripQuotes(string s)
 {
     if (s.length<2)
@@ -314,12 +386,14 @@ string stripQuotes(string s)
     return s;
 }
 
+///
 string fixUrl(string s) // sorry - not sure why urls are being escaped - kludge for now
 {
     import std.string:replace;
     return s.replace("\\/","/");
 }
 
+///
 auto request(PushoverAPI api, string url, HTTP.Method method=HTTP.Method.get, JSONValue params=JSONValue(null))
 {
     import std.array:appender;
@@ -373,6 +447,7 @@ auto request(PushoverAPI api, string url, HTTP.Method method=HTTP.Method.get, JS
 
 
     
+///
 unittest
 {
     import std.datetime:Clock;
